4c69f47b33307b26f81602880791ae9a0b1395df
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-02-18 19:57:06 -0330

    function separate

--- site.js
+++ site.js
@@ -5,14 +5,15 @@ arrayJSON = new Array('1', '2');
 
 fetch('csvtoupload/Retro Goon League.csv')
     .then(data => data.text())
-    .then(result => {
-        return result.slice(result.indexOf("\n") + 1).split("\n").forEach(value => {
-            return result.slice(0, result.indexOf("\n")).split(",").forEach(value2 => {
-                return arrayJSON.push(value2, value);
-            });
+    .then(result => {csvToJSON(result)})
+    .then(furthermore => console.log(furthermore));
+
+function csvToJSON(csv){
+    arrayJSON = new Array('1', '2');
+    csv.slice(result.indexOf("\n") + 1).split("\n").forEach(value => {
+        csv.slice(0, result.indexOf("\n")).split(",").forEach(value2 => {
+            arrayJSON.push(value2, value);
         });
-    })
-    .then(furthermore => console.log(arrayJSON.stringify()));
-
-
-
+    });
+    return arrayJSON.stringify();
+}
