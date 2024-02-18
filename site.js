8a870474399799f466da81ecf1932b04c0e630e5
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-02-18 19:24:00 -0330

    removed rows.split

--- site.js
+++ site.js
@@ -8,7 +8,6 @@ fetch('csvtoupload/Retro Goon League.csv')
     .then(result => {
         let headers = result.slice(0, result.indexOf("\n")).split(",");
         let rows = result.slice(result.indexOf("\n") + 1).split("\n");
-        let elements = rows.split(",");
         rows.forEach(value => {
             headers.forEach.split(",")(value2 => {
                 arrayJSON.push(value2, value);
