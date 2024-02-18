41a38b7aaac0cb4e9ef91ccd7cd6c4b4bbd8379c
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-02-18 19:59:11 -0330

    foreach csv

--- site.js
+++ site.js
@@ -10,8 +10,8 @@ fetch('csvtoupload/Retro Goon League.csv')
 
 function csvToJSON(csv){
     arrayJSON = new Array('1', '2');
-    csv.slice(result.indexOf("\n") + 1).split("\n").forEach(value => {
-        csv.slice(0, result.indexOf("\n")).split(",").forEach(value2 => {
+    csv.slice(csv.indexOf("\n") + 1).split("\n").forEach(value => {
+        csv.slice(0, csv.indexOf("\n")).split(",").forEach(value2 => {
             arrayJSON.push(value2, value);
         });
     });
