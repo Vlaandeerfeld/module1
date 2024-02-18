3bfeaf3e4c131e02b1762a64e2a28771f43b64e6
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-02-18 18:49:41 -0330

    result.json console.log JSON

--- site.js
+++ site.js
@@ -1,5 +1,10 @@
 let fs = new FileReader();
+let league;
 
 fetch('csvtoupload/Retro Goon League.csv')
     .then(data => data.text())
-    .then(result => console.log(result))
+    .then(result => league = result.json())
+    .then(console.log(league))
+
+
+
