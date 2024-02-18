a146c60f20fe3f95eec222153bb8b8fb2e50a4b2
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-02-18 19:36:47 -0330

    Removed variables

--- site.js
+++ site.js
@@ -6,10 +6,8 @@ arrayJSON = new Array('1', '2');
 fetch('csvtoupload/Retro Goon League.csv')
     .then(data => data.text())
     .then(result => {
-        let headers = result.slice(0, result.indexOf("\n")).split(",");
-        let rows = result.slice(result.indexOf("\n") + 1).split("\n");
-        return rows.forEach(value => {
-            return headers.forEach(value2 => {
+        return result.slice(result.indexOf("\n") + 1).split("\n").forEach(value => {
+            return result.slice(0, result.indexOf("\n")).split(",").forEach(value2 => {
                 return arrayJSON.push(value2, value);
             });
         });
