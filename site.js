6415e49d381f84d41495eda03a6c6a128186d707
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-02-18 19:09:56 -0330

    changed split by \n

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ fetch('csvtoupload/Retro Goon League.csv')
     .then(data => data.text())
     .then(result => {
         let headers = result.slice(0, result.indexOf("\n")).split(",");
-        let rows = result.slice(result.indexOf("\n") + 1).split(",");
+        let rows = result.slice(result.indexOf("\n") + 1).split("\n");
         console.log(rows);
     });
 
