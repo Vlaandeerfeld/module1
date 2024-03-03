8e2cfed2f010ef52c4d6ac662128b2715f2da2fd
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-03-03 16:35:09 -0330

    ADDED to teamStats.html

--- site.js
+++ site.js
@@ -136,18 +136,19 @@ function seasonFilters(league){
 
 function teamTablesOverview(league, season, team){
 	let data = JSON.parse(localStorage[league]);
-    console.log(data);
+	league = league.toString();
+    console.log(league);
 	let outputHTML = '';
 	outputHTML += `<tbody><tr><th onclick = 'sortTable("teamTablesOverview", 0)'>Team</th><th onclick = 'sortTableNum("teamTablesOverview", 1)'>GP</th><th onclick = 'sortTableNum("teamTablesOverview", 2)'>Wins</th><th onclick = 'sortTableNum("teamTablesOverview", 3)'>Losses</th><th onclick = 'sortTableNum("teamTablesOverview", 4)'>Ties</th><th onclick = 'sortTableNum("teamTablesOverview", 5)'>OTL</th><th onclick = 'sortTableNum("teamTablesOverview", 6)'>Points</th><th onclick = 'sortTableNum("teamTablesOverview", 7)'>PCT</th><th onclick = 'sortTableNum("teamTablesOverview", 8)'>G</th></tr>`;
 	data.forEach(value => {
 		if (team != 'ALL'){
 			if (value.Season == season && value.Abbr == team){
-				outputHTML += `<tr><td onclick = 'window.location.href = "FHMstatsteam.html"; ${localStorage["currentLeague"]} = ${league}; 'localStorage["currentTeam"] = ${value.Abbr}'>${value.Abbr}</td><td>${value.GP_RS}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.Ties}</td><td>${value.OTL}</td><td>${value.Points}</td><td>${value.PCT}</td><td>${value.G_RS}</td></tr>`;
+				outputHTML += `<tr><td onclick = 'localStorage["currentLeague"] = document.getElementById("FHMleaguesstats").options[document.getElementById("FHMleaguesstats").options.selectedIndex].text; localStorage["currentTeam"] = document.getElementById("FHMteamsstats").options[document.getElementById("FHMteamsstats").options.selectedIndex].text; window.location.href = "FHMstatsteam.html"'>${value.Abbr}</td><td>${value.GP_RS}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.Ties}</td><td>${value.OTL}</td><td>${value.Points}</td><td>${value.PCT}</td><td>${value.G_RS}</td></tr>`;
 			}
 		}
 		else{
 			if (value.Season == season){
-				outputHTML += `<tr><td onclick = 'window.location.href = "FHMstatsteam.html";' 'localStorage.setItem("currentLeague", league);' 'localStorage["currentTeam"] = ${value.Abbr}'>${value.Abbr}</td><td>${value.GP_RS}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.Ties}</td><td>${value.OTL}</td><td>${value.Points}</td><td>${value.PCT}</td><td>${value.G_RS}</td></tr>`;
+				outputHTML += `<tr><td id = ${value.Abbr} onclick = 'localStorage["currentLeague"] = document.getElementById("FHMleaguesstats").options[document.getElementById("FHMleaguesstats").options.selectedIndex].text; localStorage.setItem("currentTeam", document.getElementById("${value.Abbr}").innerHTML); window.location.href = "FHMstatsteam.html";'>${value.Abbr}</td><td>${value.GP_RS}</td><td>${value.Wins}</td><td>${value.Losses}</td><td>${value.Ties}</td><td>${value.OTL}</td><td>${value.Points}</td><td>${value.PCT}</td><td>${value.G_RS}</td></tr>`;
 			}
 		}
 	});
@@ -159,13 +160,95 @@ function teamTablesOverview(league, season, team){
 function teamStatsFHM(){
 	console.log(localStorage['currentLeague']);
 	console.log(localStorage['currentTeam']);
-	let data = localStorage[localStorage['currentLeague']];
+	let data = JSON.parse(localStorage[localStorage['currentLeague']]);
 	let team = localStorage['currentTeam'];
+	let outputHTMLRoster = '';
+	let outputHTMLRosterStats = '';
+	let outputHTMLLinesStats = '';
 
+	outputHTMLRoster += `</tbody>`;
+	outputHTMLRosterStats += `</tbody>`;
+	outputHTMLLinesStats += `</tbody>`;
 	data.forEach(value => {
 		if (value.Abbr == team){
-			document.getElementById('navButtonHomeFHMstatsteam').style.backgroundColor = value['Primary Color'];
+
+			console.log(value['Primary Color']);
+			console.log(value['Secondary Color']);
+			document.getElementById('FHMTeamLogo').style.backgroundColor = value['Secondary Color'];
+			document.getElementById('FHMTeamLogo').style.Color = value['Primary Color'];
+			document.getElementById('FHMTeamLogo').style.fontSize = '100px';
+			document.getElementById('FHMTeamLogo').innerHTML = value['Name'] + ' ' + value['Nickname'];
+			outputHTMLRoster += `
+					<tr><th>LW</th><th>C</th><th>RW</th>
+					<tr><th>${value['LW1_line']}</th><th>${value['C1_line']}</th><th>${value['RW1_line']}</th></tr>
+					<tr><th>${value['LW2_line']}</th><th>${value['C2_line']}</th><th>${value['RW2_line']}</th></tr>
+					<tr><th>${value['LW3_line']}</th><th>${value['C3_line']}</th><th>${value['RW3_line']}</th></tr>
+					<tr><th>${value['LW4_line']}</th><th>${value['C4_line']}</th><th>${value['RW4_line']}</th></tr>
+					<tr><th>LD</th><th>RD</th>
+					<tr><th>${value['LD1_line']}</th><th>${value['RD1_line']}</th></tr>
+					<tr><th>${value['LD2_line']}</th><th>${value['RD2_line']}</th></tr>
+					<tr><th>${value['LD3_line']}</th><th>${value['RD3_line']}</th></tr>
+					<tr><th>Main</th><th>Backup</th>
+					<tr><th>${value['G1']}</th><th>${value['G2']}</th></tr>
+			`;
+
+			outputHTMLRosterStats += `<tr><th>Name</th><th>Points</th><th>G/60</th>`;
+
+			let F1Points = 0;
+			let F2Points = 0;
+			let F3Points = 0;
+			let F4Points = 0;
+			let D1Points = 0;
+			let D2Points = 0;
+			let D3Points = 0;
+
+			for(let count = 0; count <= 25; count++){
+
+
+				if(value['PlayerId' + count.toString()] != undefined){
+					outputHTMLRosterStats += `
+					<tr><td>${value['First Name' + count.toString()]} ${value['Last Name' + count.toString()]}</td><td>` + (Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()])) + `</td><td>${value['GF/60_RS' + count.toString()]}</td></tr>
+					`;
+				}
+				if(value['Last Name' + count.toString()] == value['LW1_line'] || value['Last Name' + count.toString()] == value['C1_line'] || value['Last Name' + count.toString()] == value['RW1_line']){
+					F1Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+				if(value['Last Name' + count.toString()] == value['LW2_line'] || value['Last Name' + count.toString()] == value['C2_line'] || value['Last Name' + count.toString()] == value['RW2_line']){
+					F2Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+				if(value['Last Name' + count.toString()] == value['LW3_line'] || value['Last Name' + count.toString()] == value['C3_line'] || value['Last Name' + count.toString()] == value['RW3_line']){
+					F3Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+				if(value['Last Name' + count.toString()] == value['LW4_line'] || value['Last Name' + count.toString()] == value['C4_line'] || value['Last Name' + count.toString()] == value['RW4_line']){
+					F4Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+				if(value['Last Name' + count.toString()] == value['LD1_line'] || value['Last Name' + count.toString()] == value['RD1_line']){
+					D1Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+				if(value['Last Name' + count.toString()] == value['LD2_line'] || value['Last Name' + count.toString()] == value['RD2_line']){
+					D2Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+				if(value['Last Name' + count.toString()] == value['LD3_line'] || value['Last Name' + count.toString()] == value['RD3_line']){
+					D3Points += Number(value['G_RS' + count.toString()]) + Number(value['A_RS' + count.toString()]);
+				}
+			}
+			outputHTMLLinesStats += `
+					<tr><th>Offense</th><th>Points</th><th>Team%</th></tr>
+					<tr><td>1</td><td>${F1Points}</td></tr>
+					<tr><td>2</td><td>${F2Points}</td></tr>
+					<tr><td>3</td><td>${F3Points}</td></tr>
+					<tr><td>4</td><td>${F4Points}</td></tr>
+					<tr><th>Defense</th><th>Points</th><th>Team%</th></tr>
+					<tr><td>1</td><td>${D1Points}</td></tr>
+					<tr><td>2</td><td>${D2Points}</td></tr>
+					<tr><td>3</td><td>${D3Points}</td></tr>
+					`
 		}
+		outputHTMLRoster += `</tbody>`;
+		outputHTMLRosterStats += `</tbody>`;
+		document.getElementById('FHMstatsteamRoster').innerHTML = outputHTMLRoster;
+		document.getElementById('FHMstatsteamRosterStats').innerHTML = outputHTMLRosterStats;
+		document.getElementById('FHMstatsteamLinesStats').innerHTML = outputHTMLLinesStats;
 	})
 }
 
