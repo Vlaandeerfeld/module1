2df526de86f7ff7b352603dfd0a02618c061750c
Michael Young <58713796+Vlaandeerfeld@users.noreply.github.com>
2024-04-28 14:14:18 -0230

    UPDATED mainpage UI and tried to fix cors policy

--- NHL.js
+++ NHL.js
@@ -108,7 +108,7 @@ function sorterTeams(table){
 
 async function getAPI(url){
 
-	const response = await fetch(url);
+	const response = await fetch('https://corsproxy.io/?'+ url);
 	const result = await response.json()
 	return result;
 }
